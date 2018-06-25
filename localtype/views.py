62f5789d5afdd8fc7701c599ba0706d68a64b19b
Nicholas Knezek <nknezek@gmail.com>
2018-06-25 11:30:13 -0700

    correct references

--- localtype/views.py
+++ localtype/views.py
@@ -23,8 +23,8 @@ def tokenize(text):
 
 try:
     base_dir = '/home/ubuntu/localtype_site/localtype/data/'
-    tfidf_vectorizer = dill.load(open(base_dir+'tfidf_vectorizer_nlpimport2.m', 'rb'))
-    c = dill.load(open(base_dir+"trained_pipeline_nlpimport2.m", 'rb'))
+    tfidf_vectorizer = dill.load(open(base_dir+'tfidf_vectorizer_nlpimport.m', 'rb'))
+    c = dill.load(open(base_dir+"trained_pipeline_nlpimport.m", 'rb'))
 except:
     print('first import failed, trying secondary import')
     base_dir = '/Users/nknezek/Documents/Insight_local/localtype_site/localtype/data/'
